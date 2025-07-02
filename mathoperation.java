332c1749dc8703c5b9614e5c78b1fc296a57d7c6
Yaman2508 <yamankumar2508@gmail.com>
2025-07-02 12:56:12 +0530

    Add files via upload

---
+++ mathoperation.java
@@ -0,0 +1,15 @@
+ class mathoperation {
+    public static void main (String []args){
+        int a = 20;
+        int b = 14;
+        int sum = a + b;
+        int diff = a - b;
+        int multiply = a * b;
+        int divide = a / b;
+        System.out.println("sum = " + sum);
+        System.out.println("diff = " + diff);
+        System.out.println("multiply = " + multiply);
+        System.out.println("divide = " + divide);
+    }
+    
+}
